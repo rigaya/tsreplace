e28a0f280c3ff953c902cb733279e598b0aab5f9
rigaya <rigaya34589@live.jp>
2026-03-15 10:10:08 +0900

    パッケージビルドを簡素化。

--- build_deb.sh
+++ build_deb.sh
@@ -13,11 +13,12 @@ if [ -e /etc/lsb-release ]; then
     PACKAGE_OS_ID=`cat /etc/lsb-release | grep DISTRIB_ID | cut -f 2 --delim="="`
     PACKAGE_OS_VER=`cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -f 2 --delim="="`
     PACKAGE_OS_CODENAME=`cat /etc/lsb-release | grep DISTRIB_CODENAME | cut -f 2 --delim="="`
-    PACKAGE_OS="_${PACKAGE_OS_ID}${PACKAGE_OS_VER}"
-    if [ "${PACKAGE_OS_CODENAME}" = "focal" ] || [ "${PACKAGE_OS_CODENAME}" = "jammy" ]; then
-        PACKAGE_DEPENDS="libc6(>=2.29),libgcc-s1"
+    if [ "${PACKAGE_OS_CODENAME}" = "focal" ]; then
+        PACKAGE_DEPENDS="libc6(>=2.31),libgcc-s1"
+    elif [ "${PACKAGE_OS_CODENAME}" = "jammy" ]; then
+        PACKAGE_DEPENDS="libc6(>=2.31),libgcc-s1"
     elif [ "${PACKAGE_OS_CODENAME}" = "noble" ]; then
-        PACKAGE_DEPENDS="libc6(>=2.29),libgcc-s1"
+        PACKAGE_DEPENDS="libc6(>=2.31),libgcc-s1"
     else
         echo "${PACKAGE_OS_ID}${PACKAGE_OS_VER} ${PACKAGE_OS_CODENAME} not supported in this script!"
         exit 1
@@ -45,5 +46,5 @@ mkdir -p ${PACKAGE_ROOT}/usr/bin
 cp ${PACKAGE_BIN} ${PACKAGE_ROOT}/usr/bin
 chmod +x ${PACKAGE_ROOT}/usr/bin/${PACKAGE_BIN}
 
-DEB_FILE="${PACKAGE_NAME}_${PACKAGE_VERSION}${PACKAGE_OS}_${PACKAGE_ARCH}.deb"
+DEB_FILE="${PACKAGE_NAME}_${PACKAGE_VERSION}_${PACKAGE_ARCH}.deb"
 dpkg-deb -b "${PACKAGE_ROOT}" "${DEB_FILE}"
